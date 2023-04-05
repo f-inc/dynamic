0e0a1a9fb4e4f75cd8d14ac7c7e70de182d6ff6d
Furqan Rydhan <furqan.rydhan@gmail.com>
2023-04-05 00:15:57 +0000

    initial sketch

---
+++ src/packages/python/main.py
@@ -0,0 +1,4 @@
+from protocol.http import start
+
+if __name__ == "__main__":
+    start(host="localhost", port=8080)
---
+++ src/packages/python/protocol/http.py
@@ -0,0 +1,5 @@
+def start(
+    host="localhost",
+    port=8080,
+):
+    print(f"Starting HTTP server on {host}:{port}")
