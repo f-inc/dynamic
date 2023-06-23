91f23c6a473aed99e2df36a2245c1a8fcd3eaadf
Aman Ibrahim <amanmibra@gmail.com>
2023-06-22 17:55:07 -0700

    Move example

---
+++ packages/python/dynamic/runners/langchain/example/agent.py
@@ -0,0 +1,32 @@
+"""
+Example Script
+"""
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.llms import OpenAI
+from langchain.agents.agent_toolkits import NLAToolkit
+
+from dynamic.runners.langchain import AgentRunner, AgentRunnerConfig
+
+if __name__ == "__main__":
+    llm = OpenAI(
+        client=None,
+        temperature=0.9,
+    )
+    tools = NLAToolkit.from_llm_and_url(llm, "https://api.speak.com/openapi.yaml").get_tools()
+
+    agent = initialize_agent(
+        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    )
+
+    agent_input = dict(input="What does \"donde esta la biblioteca?\" mean? And what is a way to respond to this?")
+    config = AgentRunnerConfig(agent_input=agent_input)
+
+    runner = AgentRunner(agent, config)
+
+    print("Runner created and running...")
+    print(runner.run())
