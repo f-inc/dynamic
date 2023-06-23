3d4bc943dc9dc9b18549b368fc6f10406e1953c1
Aman Ibrahim <amanmibra@gmail.com>
2023-06-22 17:54:15 -0700

    Add dynamicagent to get_runner

--- packages/python/dynamic/runners/utils.py
+++ packages/python/dynamic/runners/utils.py
@@ -3,15 +3,17 @@ from typing import Any, Tuple
 from dynamic.runners.runner import Runner
 from dynamic.runners.callable import CallableRunner, CallableRunnerConfig
 from dynamic.runners.langchain import AgentRunner, AgentRunnerConfig, ChainRunner, ChainRunnerConfig
+from dynamic.classes.agent import DynamicAgent
 
 from langchain.agents import Agent, AgentExecutor
 from langchain.chains.base import Chain
 
+
 def get_runner(handle: Any) -> Tuple[Runner, Any]:
-    if isinstance(handle, Chain):
-        return ChainRunner, ChainRunnerConfig
-    elif isinstance(handle, Agent) or isinstance(handle, AgentExecutor):
+    if isinstance(handle, (Agent, AgentExecutor, DynamicAgent)):
         return AgentRunner, AgentRunnerConfig
+    elif isinstance(handle, Chain):
+        return ChainRunner, ChainRunnerConfig
     elif callable(handle):
         return CallableRunner, CallableRunnerConfig
     
