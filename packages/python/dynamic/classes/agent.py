33b5f0b6c21ac6a30b767cb303114eb82603a000
Aman Ibrahim <amanmibra@gmail.com>
2023-06-23 17:03:27 -0700

    Add server message to Dynamic Agent callback

--- packages/python/dynamic/classes/agent.py
+++ packages/python/dynamic/classes/agent.py
@@ -8,6 +8,9 @@ from fastapi import WebSocket
 from langchain.agents import load_tools, initialize_agent
 from langchain.callbacks.base import AsyncCallbackHandler
 
+# dynamic
+from dynamic.classes.message import ServerMessage
+
 
 class DynamicAgent:
     def __init__(self, llm, **kwargs):
@@ -37,4 +40,7 @@ class WebsocketCallbackHandler(AsyncCallbackHandler):
         self.websocket = websocket
 
     async def on_llm_new_token(self, token: str, **kwargs) -> None:
-        await self.websocket.send_json(dict(data=token))
+        messsage = ServerMessage(
+            content=token
+        )
+        await self.websocket.send_json(messsage.to_dict())
