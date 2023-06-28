2f89614b8b91457e6943691a03f142f1d16fa83f
Aman Ibrahim <amanmibra@gmail.com>
2023-06-28 12:26:34 -0700

    Example app for testing

---
+++ packages/python/examples/example_app/routes/__init__.py
@@ -0,0 +1,12 @@
+def get_foo():
+    return 'foo'
+
+def delete_foo():
+    return 'foo-delete'
+
+class User:
+    def __init__():
+        pass
+
+    def test():
+        pass
---
+++ packages/python/examples/example_app/routes/user.py
@@ -0,0 +1,15 @@
+from dynamic.router.decorator import dynamic
+
+@dynamic
+def get_foo():
+    return 'foo'
+
+def delete_foo():
+    return 'foo-delete'
+
+class User:
+    def __init__():
+        pass
+
+    def test():
+        pass
