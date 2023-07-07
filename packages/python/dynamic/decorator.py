cfcb21621f031c49478a9e4b85ecf09992e504b1
Aman Ibrahim <amanmibra@gmail.com>
2023-07-06 22:18:34 -0400

    Support decorator without args

--- packages/python/dynamic/decorator.py
+++ packages/python/dynamic/decorator.py
@@ -1,8 +1,11 @@
 from functools import wraps
+import logging
 
-def dynamic(streaming=False):
+from dynamic.classes.agent import DynamicAgent
+
+def dynamic(func=None, streaming=False):
     def decorator(func):
-
+        # logging.info(f"test {func.__name__}")
         @wraps(func)
         def wrapper(*args, **kwargs):
             output = func(*args, **kwargs)
@@ -12,6 +15,11 @@ def dynamic(streaming=False):
                     raise Exception("Streaming endpoints must return DynamicAgents")
 
             return output
+
         return wrapper
+
+    if callable(func):
+        return decorator(func)    
+
     return decorator
 
