2887900bc2800331d2426d9a9c9448f802665fc0
Aman Ibrahim <amanmibra@gmail.com>
2023-07-07 09:02:36 -0400

    docstrings

--- packages/python/dynamic/router/get_file_routes.py
+++ packages/python/dynamic/router/get_file_routes.py
@@ -69,6 +69,7 @@ def _get_valid_module_functions(package):
     return None
 
 def _get_package_contents(file_path):
+    """Import functions and other modules within a file"""
     spec = importlib.util.spec_from_file_location(file_path, location=file_path)
     module = importlib.util.module_from_spec(spec)
     sys.modules["module.name"] = module
@@ -78,20 +79,17 @@ def _get_package_contents(file_path):
     return module
 
 def _get_route_name(file_path):
+    """Given a file path, return a valid endpoint
+
+    Example: routes/foo/bar.py --> foo/bar
+    """
     path = file_path.split(MODULE_EXTENSIONS)[0]
     route_path = path.split(DEFAULT_ROUTES_DIRECTORY)[1]
     return route_path
 
-def _get_list_of_routes():
-    route_dir_path = _get_route_dir_path()
 
-    _, files = _run_fast_scandir(route_dir_path, [".py"])
-
-    files = [f for f in files if "__" not in f]
-
-    return files
-
-def _run_fast_scandir(dir, ext):    # dir: str, ext: list
+def _run_fast_scandir(dir, ext=[MODULE_EXTENSIONS]):
+    """Recurseively find all files in a directory, while also given desired file extension."""
     subfolders, files = [], []
 
     for f in os.scandir(dir):
@@ -108,8 +106,3 @@ def _run_fast_scandir(dir, ext):    # dir: str, ext: list
         files.extend(f)
     return subfolders, files
 
-def _get_route_dir_path():
-    path = os.path.dirname(os.path.abspath(sys.argv[0]))
-    path = os.path.normpath(path + DEFAULT_ROUTES_DIRECTORY)
-
-    return path
