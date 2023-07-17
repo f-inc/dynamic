8cadd70b0ef98b5d2c4e6c7d4ae89920ded9dcd7
Aman Ibrahim <amanmibra@gmail.com>
2023-07-17 15:45:51 -0700

    Update parsing and handle chat conversational type

--- packages/python/dynamic/classes/dynamic_agent.py
+++ packages/python/dynamic/classes/dynamic_agent.py
@@ -1,20 +1,25 @@
 import logging
 from typing import Any, List, Optional
+import json
+from uuid import UUID
 
 # fastapi
 from fastapi import WebSocket
 
 # langchain
-from langchain.agents import load_tools, initialize_agent
+from langchain.agents import AgentType, load_tools, initialize_agent
 from langchain.callbacks.base import AsyncCallbackHandler
+from langchain.schema import LLMResult
+from langchain.agents.conversational_chat.output_parser import ConvoOutputParser
 
 # dynamic
 from dynamic.classes.message import ServerMessage
 
 
 class DynamicAgent:
-    def __init__(self, llm, **kwargs):
+    def __init__(self, llm, agent, **kwargs):
         self.llm = llm
+        self.agent = agent
         self.kwargs = kwargs
 
     def _initialize_agent_with_websocket(self, websocket: WebSocket):
@@ -26,12 +31,25 @@ class DynamicAgent:
         llm.verbose = True
         llm.callbacks = [WebsocketCallbackHandler(websocket)]
 
+        # chat agent
+        if self.agent == AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION:
+            agent_kwargs = self.kwargs.get("agent_kwargs", {})
+            agent_kwargs["output_parser"] = DynamicParser()
+            self.kwargs["agent_kwargs"] = agent_kwargs
+
         tool_list = self.kwargs.get("tool_list")
         if tool_list:
             self.kwargs["tools"] = load_tools(tool_list, llm=llm)
 
         logging.info("Initializing agent...")
         return initialize_agent(llm=llm, **self.kwargs)
+
+class DynamicParser(ConvoOutputParser):
+    def parse(self, text: str):
+        if "action" not in text:
+            text = json.dumps(dict(action="Final Answer", action_input=text))
+
+        return super().parse(text)
 
 
 class WebsocketCallbackHandler(AsyncCallbackHandler):
