c4b520fd070f6eb73d28ca246c9ed0f1add24e0f
Aman Ibrahim <amanmibra@gmail.com>
2023-07-17 15:46:03 -0700

    Update example

--- packages/python/example/agent.py
+++ packages/python/example/agent.py
@@ -1,9 +1,15 @@
+
+import logging
+from typing import Union
 from dotenv import load_dotenv
+from langchain.schema import AgentAction, AgentFinish
 
 load_dotenv()
 
 from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
 
 from dynamic.classes.dynamic_agent  import DynamicAgent
 
@@ -18,4 +24,16 @@ inline_agent = initialize_agent(
 
 streaming_agent = DynamicAgent(
     tool_list=tool_list, llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True,
+)
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+chat_llm = ChatOpenAI(temperature=0, streaming=True)
+
+chat_agent = DynamicAgent(
+    tool_list=tool_list,
+    llm=chat_llm,
+    agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+    verbose=True,
+    memory=memory,
 )
