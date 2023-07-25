c5f01e09c760f35474d4e27367c2eb72318f68e5
Aman Ibrahim <amanmibra@gmail.com>
2023-07-24 20:14:18 -0700

    lint

--- packages/js/dynamic/src/dynamic.ts
+++ packages/js/dynamic/src/dynamic.ts
@@ -1,26 +1,26 @@
 // fastify
-import Fastify, { type FastifyInstance } from "fastify";
-import FastifyWebsocket from "@fastify/websocket";
+import Fastify, { type FastifyInstance } from 'fastify'
+import FastifyWebsocket from '@fastify/websocket'
 
 // default plugins
-import autoRoute from "fastify-autoroutes";
+import autoRoute from 'fastify-autoroutes'
 
-type DynamicOptions = {
-  fileBased?: boolean;
-};
+interface DynamicOptions {
+  fileBased?: boolean
+}
 
-const dynamic = (options?: DynamicOptions) => {
+const dynamic = (options?: DynamicOptions): FastifyInstance => {
   const app: FastifyInstance = Fastify({
-    logger: true,
-  });
+    logger: true
+  })
 
-  app.register(FastifyWebsocket);
+  app.register(FastifyWebsocket)
 
   app.register(autoRoute, {
-    dir: "./../routes",
-  });
+    dir: './../routes'
+  })
 
-  return app;
-};
+  return app
+}
 
-export default dynamic;
+export default dynamic
