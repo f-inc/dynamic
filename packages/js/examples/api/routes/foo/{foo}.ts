e33e540ddabb9a4c19643227e07c5c17374f1a14
Aman Ibrahim <amanmibra@gmail.com>
2023-07-25 08:35:02 -0700

    Added put example

--- packages/js/examples/api/routes/foo/{foo}.ts
+++ packages/js/examples/api/routes/foo/{foo}.ts
@@ -2,17 +2,26 @@
 import type { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import { Resource } from "fastify-autoroutes";
 
-interface GetFooParams {
+interface FooParams {
   foo: string;
 }
 
-interface GetFooRequest extends FastifyRequest {
-  params: GetFooParams;
+interface FooBody {
+  data: string;
+}
+
+interface FooRequest extends FastifyRequest {
+  params: FooParams;
+  body: FooBody;
 }
 export default (fastify: FastifyInstance) =>
   <Resource>{
     get: {
-      handler: async (request: GetFooRequest, reply: FastifyReply) =>
+      handler: async (request: FooRequest, reply: FastifyReply) =>
         `Foo Foo ${request.params.foo}!`,
+    },
+    put: {
+      handler: async (request: FooRequest, reply: FastifyReply) =>
+        `put body: ${request.body.data}! \nput params: ${request.params.foo}\n`,
     },
   };
