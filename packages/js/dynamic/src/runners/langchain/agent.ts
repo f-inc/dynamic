f03a86d9db81d8f3bac606e9057532dfe216bb68
Aman Ibrahim <amanmibra@gmail.com>
2023-07-28 15:51:29 -0700

    Add AgentExecutor supprot

--- packages/js/dynamic/src/runners/langchain/agent.ts
+++ packages/js/dynamic/src/runners/langchain/agent.ts
@@ -1,24 +1,25 @@
 // dynamic
 import { Runner, type Config } from '../types';
 import { DynamicAgent } from '../../dync/langchain/agent';
+import { AgentExecutor } from 'langchain/agents';
 
 export class AgentRunner extends Runner {
   streaming: boolean; // marker to indicate agent will stream its tokens
 
   constructor(
-    handle: DynamicAgent,
+    handle: DynamicAgent | AgentExecutor,
     config: Config,
     websocket?: WebSocket,
     streaming?: boolean
   ) {
     super(handle, config);
     this.streaming = streaming ?? false;
+    if (this.streaming) {
     if (!(handle instanceof DynamicAgent)) {
       throw new Error(
         `"handle" expected DynamicAgent, recieved ${typeof handle}`
       );
     }
-    if (this.streaming) {
       if (websocket == null) {
         throw new Error(
           'DynamicAgent marked as `streaming` without a websocket.'
