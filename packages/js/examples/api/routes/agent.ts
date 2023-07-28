a9553c86398fd6fa61e7b63c590cebf58e7124f6
Aman Ibrahim <amanmibra@gmail.com>
2023-07-27 18:19:02 -0700

    Add example

---
+++ packages/js/examples/api/routes/agent.ts
@@ -0,0 +1,43 @@
+// dynamic
+import { DynamicAgent } from "./../../../dynamic/src/dync/langchain/agent";
+
+// fastify
+import type { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+import { Resource } from "fastify-autoroutes";
+
+// langchain
+import { type Tool } from "langchain/dist/tools/base";
+import { OpenAI } from "langchain/llms/openai";
+import { SerpAPI } from "langchain/tools";
+import { Calculator } from "langchain/tools/calculator";
+
+/** Defined Agent */
+
+const model = new OpenAI({
+  temperature: 0,
+});
+const tools: Tool[] = [
+  new SerpAPI(undefined, {
+    location: "San Francisco, California, USA",
+    hl: "en",
+    gl: "us",
+  }),
+  new Calculator(),
+];
+
+const agentHandler = async () => {
+  return new DynamicAgent(tools, model, {
+    agentType: "zero-shot-react-description",
+    verbose: true,
+  });
+};
+
+/** Define Route */
+
+export default (fastify: FastifyInstance) =>
+  <Resource>{
+    get: {
+      handler: () => null,
+      wsHandler: agentHandler,
+    },
+  };
